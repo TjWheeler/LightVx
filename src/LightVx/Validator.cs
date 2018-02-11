2c4e0050b6dd90e37d9fafec42e814ac07b04f2f
Tim Wheeler <tj.wheeler@me.com>
2018-02-11 12:30:30 +1100

    Updating documentation

--- src/LightVx/Validator.cs
+++ src/LightVx/Validator.cs
@@ -1,37 +1,59 @@
 //   Author: Tim Wheeler
 //   GitHub: https://github.com/TjWheeler/LightVx
-//   Contains:    User Input Validation - Reusable and Extendable classes
+//   Purpose: User Input Validation - Web Service / Web API Input Validation
 //   Description: A simple validation framework.  Intended to validate user input. 
 //              : Also useful for validating webservice or webapi input.
 //              : Validators can be combined using the AggregatedValidator.
 
 
 //Examples:
-//		Quick syntax example using Validator helper:
-// 		string errorMessage;
-//      if (Validator.IsNotValid<AlphaTextValidator>(input, nameof(account), out errorMessage))
-//      {
-//         return BadRequest($"The input is invalid: {errorMessage}");
-//      }
-//		 
-//       private void Example1Numeric()
-//       {
-//           string input = "123ABC";
-//           IInputValidator validator = new NumericValidator();
-//           validator.Input = input;
-//           validator.Validate();
-//           if (!validator.IsValid)
-//               Console.WriteLine("Input: " + input + " return error (" + validator.ErrorMessage + ")");
-//       }
-//       private void Example2ControlName()
-//       {   //This example adds the Control Name to a validate function call which incorporates it
-//           //in the user error message;
-//           string input = "123ABC";
-//           IInputValidator validator = new NumericValidator();
-//           string userErrorMessage;
-//           if(!validator.Validate(input,"Phone Number", out userErrorMessage))
-//               Console.WriteLine(userErrorMessage);
-//       }   
+/*
+		Example 1: Quick syntax example using Validator helper:
+ 		string errorMessage;
+        if (Validator.IsNotValid<AlphaTextValidator>(input, nameof(account), out errorMessage))
+        {
+            return BadRequest($"The input is invalid: {errorMessage}");
+        }
+
+		Example 2: Fluent API example:		 
+        public void Example2Test()
+        {
+            string input = "ABCD";
+            bool? isValid = Validator.Eval(input, "MyFieldName")
+                    .Required()
+                    .HasLength(0, 3)
+                    .IsAlphaText()
+                    .Fail(((errors, validators) =>
+                    {
+                        Console.WriteLine("Example failure: " + string.Join(";", errors));
+                        return;
+                    })).IsValid;
+            if (isValid != false)
+            {
+                Assert.Fail("This validator should have failed");
+            }
+        }
+ */
+/*   These examples use the validators directly.  For more convenience use the Validator helper.
+       private void Example1Numeric()
+       {
+           string input = "123ABC";
+           IValidator validator = new NumericValidator();
+           validator.Input = input;
+           validator.Validate();
+           if (!validator.IsValid)
+               Console.WriteLine("Input: " + input + " return error (" + validator.ErrorMessage + ")");
+       }
+       private void Example2ControlName()
+       {   //This example adds the Control Name to a validate function call which incorporates it
+           //in the user error message;
+           string input = "123ABC";
+           IValidator validator = new NumericValidator();
+           string userErrorMessage;
+           if(!validator.Validate(input,"Phone Number", out userErrorMessage))
+               Console.WriteLine(userErrorMessage);
+       }   
+*/
 
 //Extending and Combining Validators:
 //       //Uses the AggregatedValidator to combine to validation techniques into 1 validator
@@ -43,9 +65,47 @@
 //               AddValidator(new PhoneNumberValidator());
 //           }
 //       }
+
+//Addint custom validator to the Fluent API
+//Step 1: Add the custom validator
+/* public class PostCodeValidator : AggregatedValidator
+    {
+        public PostCodeValidator()
+        {
+            AddValidator(new LengthValidator(4, 4));
+            AddValidator(new NumericValidator());
+        }
+    }
+*/
+//Step 2: Add an extension method
+/*  public static class PostCodeValidatorExtension
+    {
+        public static ValidatorFluent IsPostCode(this ValidatorFluent fluentApi)
+        {
+            fluentApi.AddValidator(new PostCodeValidator());
+            return fluentApi;
+        }
+    }
+*/
+//Step 3: Call it to validate input
+/*
+    public void ExampleCustomValidaotr()
+    {
+        string input = "...";
+        bool? isValid = Validator.Eval(input, "MyFieldName")
+            .Required()
+            .IsPostCode()
+            .Fail(((errors, validators) =>
+            {
+                Console.WriteLine("Example failure: " + string.Join(";", errors));
+                return;
+            })).IsValid;
+    }
+ *
+ */
 //Notes: In most situations a validator should return success for null or empty string data.
 //       This allows for validators like the length validator to be used or combined if the field
-//       is required.  Like the ASP.Net RequiredFieldValidator.
+//       is required.  
 using System;
 using System.Diagnostics;
 
